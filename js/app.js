8f81f8b1aa627b57a7fd8079cf3af6a6bf95d400
Lucas Baptista <lucasbaptista2106@gmail.com>
2026-04-15 15:31:47 -0300

    Contado de jogos alugado no console

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
+let jogosAlugados = 0;
+
+function contarJogosAlugados(){
+    console.log(`Total de jogos alugado: ${jogosAlugados}!`);
+}
+
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
     
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.textContent = "Alugar";
-        botao.classList.remove('dashboard__item__button--return');
+
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
+        jogosAlugados++;
+
+
+        contarJogosAlugados();
     }
 }
 
